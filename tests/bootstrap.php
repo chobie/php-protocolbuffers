a051b3fe7486bb63affb0aa58fe3b26b2269714f
Shuhei Tanuma <shuhei.tanuma@gmail.com>
2013-05-14 00:07:00 +0900

    remove needless tests

--- tests/bootstrap.php
+++
@@ -1,7 +0,0 @@
-<?php
-require join(DIRECTORY_SEPARATOR, array(dirname(dirname(__FILE__)), "src","ProtocolBuffers.php"));
-require dirname(__FILE__) . DIRECTORY_SEPARATOR . "Message/Base.php";
-require dirname(__FILE__) . DIRECTORY_SEPARATOR . "Message/Addressbook.php";
-require dirname(__FILE__) . DIRECTORY_SEPARATOR . "Message/User.php";
-
-define("FIXTURE_PATH", dirname(__FILE__) . DIRECTORY_SEPARATOR . "fixtures");
