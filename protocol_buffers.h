d83a51f9b3f77b04d1683ab98e553f9dccc38975
Shuhei Tanuma <shuhei.tanuma@gmail.com>
2013-05-14 02:13:51 +0900

    oops

---
+++ protocol_buffers.h
@@ -0,0 +1,28 @@
+
+void php_protocolbuffers_init(TSRMLS_D);
+void messages_dtor(pb_scheme *entry);
+
+static int pb_get_tag_name(HashTable *proto, ulong tag, zval **result TSRMLS_DC);
+static int pb_get_msg_name(HashTable *proto, ulong tag, zval **result TSRMLS_DC);
+static int pb_tag_is_string(HashTable *proto, ulong tag TSRMLS_DC);
+static int pb_tag_is_message(HashTable *proto, ulong tag TSRMLS_DC);
+static int pb_tag_type(HashTable *proto, ulong tag TSRMLS_DC);
+static int pb_tag_wiretype(HashTable *proto, ulong tag TSRMLS_DC);
+static inline pb_scheme *pb_search_scheme_by_tag(pb_scheme* scheme, uint scheme_size, uint tag);
+static void pb_convert_msg(HashTable *proto, const char *klass, int klass_len, pb_scheme **scheme, int *size TSRMLS_DC);
+static int pb_get_scheme_container(const char *klass, size_t klass_len, pb_scheme_container **result, HashTable *descriptor TSRMLS_DC);
+static const char* pb_decode_message(INTERNAL_FUNCTION_PARAMETERS, const char *data, const char *data_end, pb_scheme_container *container, zval **result);
+
+static void pb_serializer_init(pb_serializer **serializer);
+static int pb_serializer_resize(pb_serializer *serializer, size_t size);
+static int pb_serializer_write8(pb_serializer *serializer, unsigned int value);
+static int pb_serializer_write16(pb_serializer *serializer, unsigned int value);
+static int pb_serializer_write32(pb_serializer *serializer, unsigned int value);
+static int pb_serializer_write32_le(pb_serializer *serializer, unsigned int value);
+static int pb_serializer_write64_le(pb_serializer *serializer, uint64_t value);
+static int pb_serializer_write64(pb_serializer *serializer, uint64_t value);
+static int pb_serializer_write_varint32(pb_serializer *serializer, uint8_t value);
+static int pb_serializer_write_varint64(pb_serializer *serializer, uint64_t value);
+static int pb_serializer_write_chararray(pb_serializer *serializer, unsigned char *string, size_t len);
+
+
