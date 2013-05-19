ada2386ff8f4e9e365d8ad0d54f7b58e5b7b630b
Shuhei Tanuma <shuhei.tanuma@gmail.com>
2013-05-20 01:29:51 +0900

    fix string decoding

--- protocol_buffers.cc
+++ protocol_buffers.cc
@@ -550,18 +550,25 @@ static const char* pb_decode_message(INTERNAL_FUNCTION_PARAMETERS, const char *d
                 if (value < 512) {
                     memcpy(buffer, data, value);
                     buffer[value] = '\0';
+
+                    MAKE_STD_ZVAL(dz);
+                    ZVAL_STRINGL(dz, (char*)data, value, 1);
+
+                    PHP_PB_DECOCDE_ADD_ELM
                 } else {
                     char *sub_buffer;
 
-                    sub_buffer = (char *)emalloc(value+1);
+                    sub_buffer = (char *)emalloc(value);
                     memcpy(sub_buffer, data, value);
-                    sub_buffer[value+1] = '\0';
+
+                    MAKE_STD_ZVAL(dz);
+                    ZVAL_STRINGL(dz, (char*)sub_buffer, value, 1);
+
+                    PHP_PB_DECOCDE_ADD_ELM
+
+                    efree(sub_buffer);
                 }
 
-                MAKE_STD_ZVAL(dz);
-                ZVAL_STRINGL(dz, (char*)data, value, 1);
-
-                PHP_PB_DECOCDE_ADD_ELM
             } else if (s->type == TYPE_BYTES) {
                 if (value < 512) {
                     memcpy(buffer, data, value);
