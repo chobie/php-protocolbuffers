1d1b1d7708889bc9b383aa9fd67fee45f14bd383
Shuhei Tanuma <shuhei.tanuma@gmail.com>
2013-06-01 19:31:10 +0900

    add method declaration for each classes

--- field_descriptor.cc
+++ field_descriptor.cc
@@ -1,10 +1,14 @@
 #include "php_protocol_buffers.h"
 #include "field_descriptor.h"
 
+static zend_function_entry php_protocolbuffers_field_descriptor_methods[] = {
+    {NULL, NULL, NULL}
+};
+
 void php_pb_filed_descriptor_class(TSRMLS_D)
 {
     zend_class_entry ce;
 
-    INIT_CLASS_ENTRY(ce, "ProtocolBuffers_FieldDescriptor", 0);
+    INIT_CLASS_ENTRY(ce, "ProtocolBuffers_FieldDescriptor", php_protocolbuffers_field_descriptor_methods);
     protocol_buffers_field_descriptor_class_entry = zend_register_internal_class(&ce TSRMLS_CC);
 }
--- message.cc
+++ message.cc
@@ -1,10 +1,15 @@
 #include "php_protocol_buffers.h"
 #include "message.h"
 
+
+static zend_function_entry php_protocolbuffers_message_methods[] = {
+    {NULL, NULL, NULL}
+};
+
 void php_pb_message_class(TSRMLS_D)
 {
     zend_class_entry ce;
 
-    INIT_CLASS_ENTRY(ce, "ProtocolBuffers_Message", 0);
+    INIT_CLASS_ENTRY(ce, "ProtocolBuffers_Message", php_protocolbuffers_message_methods);
     protocol_buffers_message_class_entry = zend_register_internal_class(&ce TSRMLS_CC);
 }
