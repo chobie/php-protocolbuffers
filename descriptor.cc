cb0d28eb02e037365df45e87f95e4ce31bd8de18
Shuhei Tanuma <shuhei.tanuma@gmail.com>
2013-06-04 23:51:39 +0900

    [descriptor] do not create descriptor with constructor.

--- descriptor.cc
+++ descriptor.cc
@@ -23,6 +23,9 @@ zend_object_value php_protocolbuffers_descriptor_new(zend_class_entry *ce TSRMLS
     return retval;
 }
 
+ZEND_BEGIN_ARG_INFO_EX(arginfo_pb_descriptor___construct, 0, 0, 0)
+ZEND_END_ARG_INFO()
+
 ZEND_BEGIN_ARG_INFO_EX(arginfo_pb_descriptor_get_name, 0, 0, 0)
 ZEND_END_ARG_INFO()
 
@@ -36,6 +39,15 @@ ZEND_END_ARG_INFO()
 
 ZEND_BEGIN_ARG_INFO_EX(arginfo_pb_descriptor_dump, 0, 0, 0)
 ZEND_END_ARG_INFO()
+
+
+/* {{{ proto ProtocolBuffers_Descriptor ProtocolBuffers_Descriptor::__construct()
+*/
+PHP_METHOD(protocolbuffers_descriptor, __construct)
+{
+    // do not create instance by user. we expect it always created from builder.
+}
+/* }}} */
 
 /* {{{ proto string ProtocolBuffers_Descriptor::getName()
 */
@@ -89,10 +101,11 @@ PHP_METHOD(protocolbuffers_descriptor, dump)
 /* }}} */
 
 static zend_function_entry php_protocolbuffers_descriptor_methods[] = {
-    PHP_ME(protocolbuffers_descriptor, getName,   arginfo_pb_descriptor_get_name, ZEND_ACC_PUBLIC)
-    PHP_ME(protocolbuffers_descriptor, getField,  arginfo_pb_descriptor_get_field, ZEND_ACC_PUBLIC)
-    PHP_ME(protocolbuffers_descriptor, getOption, arginfo_pb_descriptor_get_option, ZEND_ACC_PUBLIC)
-    PHP_ME(protocolbuffers_descriptor, dump,      arginfo_pb_descriptor_dump, ZEND_ACC_PUBLIC)
+    PHP_ME(protocolbuffers_descriptor, __construct, arginfo_pb_descriptor___construct, ZEND_ACC_PRIVATE)
+    PHP_ME(protocolbuffers_descriptor, getName,     arginfo_pb_descriptor_get_name, ZEND_ACC_PUBLIC)
+    PHP_ME(protocolbuffers_descriptor, getField,    arginfo_pb_descriptor_get_field, ZEND_ACC_PUBLIC)
+    PHP_ME(protocolbuffers_descriptor, getOption,   arginfo_pb_descriptor_get_option, ZEND_ACC_PUBLIC)
+    PHP_ME(protocolbuffers_descriptor, dump,        arginfo_pb_descriptor_dump, ZEND_ACC_PUBLIC)
     {NULL, NULL, NULL}
 };
 
