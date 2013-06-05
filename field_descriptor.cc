96df61bddf2933c6a23795d1ba81a20b3fe72e63
Shuhei Tanuma <shuhei.tanuma@gmail.com>
2013-06-05 18:42:00 +0900

    fix compile error

--- field_descriptor.cc
+++ field_descriptor.cc
@@ -16,7 +16,7 @@ static int php_protocolbuffers_field_descriptor_process_params(zval **zv TSRMLS_
 
 #define PHP_PROTOCOLBUFFERS_PROCESS_BOOL \
 {\
-    zend_mangle_property_name(&name, &name_length, "*", 1, key, key_length, 0);\
+    zend_mangle_property_name(&name, &name_length, (char*)"*", 1, (char*)key, key_length, 0);\
     \
     MAKE_STD_ZVAL(value);\
     if (Z_TYPE_PP(zv) != IS_BOOL) {\
@@ -43,7 +43,7 @@ static int php_protocolbuffers_field_descriptor_process_params(zval **zv TSRMLS_
                 return 1;
             }
 
-            zend_mangle_property_name(&name, &name_length, "*", 1, key, key_length, 0);
+            zend_mangle_property_name(&name, &name_length, (char*)"*", 1, (char*)key, key_length, 0);
             MAKE_STD_ZVAL(value);
             ZVAL_LONG(value, Z_LVAL_PP(zv));
             zend_hash_update(Z_OBJPROP_PP(instance), name, name_length, (void **)&value, sizeof(zval*), NULL);
@@ -58,7 +58,7 @@ static int php_protocolbuffers_field_descriptor_process_params(zval **zv TSRMLS_
         } else if (strcmp(key, "packable") == 0) {
             PHP_PROTOCOLBUFFERS_PROCESS_BOOL;
         } else if (strcmp(key, "default") == 0) {
-            zend_mangle_property_name(&name, &name_length, "*", 1, key, key_length, 0);
+            zend_mangle_property_name(&name, &name_length, (char*)"*", 1, (char*)key, key_length, 0);
 
             ZVAL_ZVAL(value, *zv, 1, 0);
             zend_hash_update(Z_OBJPROP_PP(instance), name, name_length, (void **)&value, sizeof(zval*), NULL);
