7277763cb5fe3eb7ea1c2849d32abb75f28d1394
Shuhei Tanuma <shuhei.tanuma@gmail.com>
2013-06-13 02:23:47 +0900

    [unknown field] add stub methods

--- unknown_field.c
+++ unknown_field.c
@@ -1,8 +1,62 @@
 #include "php_protocol_buffers.h"
 #include "unknown_field.h"
 
+ZEND_BEGIN_ARG_INFO_EX(arginfo_pb_unknown_field_set_number, 0, 0, 1)
+	ZEND_ARG_INFO(0, number)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_pb_unknown_field_set_type, 0, 0, 1)
+	ZEND_ARG_INFO(0, wiretype)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_pb_unknown_field_get_number, 0, 0, 0)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_pb_unknown_field_get_type, 0, 0, 0)
+ZEND_END_ARG_INFO()
+
+
+/* {{{ proto void ProtocolBuffersUnknownField::setNumber()
+*/
+PHP_METHOD(protocolbuffers_unknown_field, setNumber)
+{
+}
+/* }}} */
+
+/* {{{ proto void ProtocolBuffersUnknownField::setType()
+*/
+PHP_METHOD(protocolbuffers_unknown_field, setType)
+{
+}
+/* }}} */
+
+/* {{{ proto void ProtocolBuffersUnknownField::getNumber()
+*/
+PHP_METHOD(protocolbuffers_unknown_field, getNumber)
+{
+}
+/* }}} */
+
+/* {{{ proto void ProtocolBuffersUnknownField::getType()
+*/
+PHP_METHOD(protocolbuffers_unknown_field, getType)
+{
+}
+/* }}} */
+
 
 static zend_function_entry php_protocolbuffers_unknown_field_methods[] = {
+	PHP_ME(protocolbuffers_unknown_field, setNumber, arginfo_pb_unknown_field_set_number, ZEND_ACC_PUBLIC)
+	PHP_ME(protocolbuffers_unknown_field, setType, arginfo_pb_unknown_field_set_type, ZEND_ACC_PUBLIC)
+	PHP_ME(protocolbuffers_unknown_field, getNumber, arginfo_pb_unknown_field_get_number, ZEND_ACC_PUBLIC)
+	PHP_ME(protocolbuffers_unknown_field, getType, arginfo_pb_unknown_field_get_type, ZEND_ACC_PUBLIC)
+/*
+	PHP_ME(protocolbuffers_unknown_field, getAsVarint, arginfo_pb_unknown_field_get_as_varint, ZEND_ACC_PUBLIC)
+	PHP_ME(protocolbuffers_unknown_field, getAsFixed32, arginfo_pb_unknown_field_get_as_fixed32, ZEND_ACC_PUBLIC)
+	PHP_ME(protocolbuffers_unknown_field, getAsFixed64, arginfo_pb_unknown_field_get_as_fixed64, ZEND_ACC_PUBLIC)
+	PHP_ME(protocolbuffers_unknown_field, getAsLengthDelimited, arginfo_pb_unknown_field_get_as_fixed64, ZEND_ACC_PUBLIC)
+	PHP_ME(protocolbuffers_unknown_field, getLengthDelimitedSize, arginfo_pb_unknown_field_get_length_delimited_size, ZEND_ACC_PUBLIC)
+*/
 	{NULL, NULL, NULL}
 };
 
--- unknown_field_set.c
+++ unknown_field_set.c
@@ -1,8 +1,44 @@
 #include "php_protocol_buffers.h"
 #include "unknown_field_set.h"
 
+ZEND_BEGIN_ARG_INFO_EX(arginfo_pb_unknown_field_set_count, 0, 0, 0)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_pb_unknown_field_get_field, 0, 0, 1)
+	ZEND_ARG_INFO(0, index)
+ZEND_END_ARG_INFO()
+
+ZEND_BEGIN_ARG_INFO_EX(arginfo_pb_unknown_field_add_field, 0, 0, 1)
+	ZEND_ARG_INFO(0, unknown_field)
+ZEND_END_ARG_INFO()
+
+
+/* {{{ proto void ProtocolBuffersUnknownFieldSet::count()
+*/
+PHP_METHOD(protocolbuffers_unknown_field_set, count)
+{
+}
+/* }}} */
+
+/* {{{ proto ProtocolBuffersUnknownField ProtocolBuffersUnknownFieldSet::getField(int $index)
+*/
+PHP_METHOD(protocolbuffers_unknown_field_set, getField)
+{
+}
+/* }}} */
+
+/* {{{ proto void ProtocolBuffersUnknownFieldSet::addField(ProtocolBuffersUnknownField $field)
+*/
+PHP_METHOD(protocolbuffers_unknown_field_set, addField)
+{
+}
+/* }}} */
+
 
 static zend_function_entry php_protocolbuffers_unknown_field_set_methods[] = {
+	PHP_ME(protocolbuffers_unknown_field_set, count, arginfo_pb_unknown_field_set_count, ZEND_ACC_PUBLIC)
+	PHP_ME(protocolbuffers_unknown_field_set, getField, arginfo_pb_unknown_field_get_field, ZEND_ACC_PUBLIC)
+	PHP_ME(protocolbuffers_unknown_field_set, addField, arginfo_pb_unknown_field_add_field, ZEND_ACC_PUBLIC)
 	{NULL, NULL, NULL}
 };
 
