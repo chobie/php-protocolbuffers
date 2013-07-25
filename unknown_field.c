6218b0ebf64240eb66ddf58d9f55be9f4b8a1db7
Shuhei Tanuma <shuhei.tanuma@gmail.com>
2013-07-25 14:26:01 +0900

    [unknown field] fix indent

--- unknown_field.c
+++ unknown_field.c
@@ -415,7 +415,7 @@ static zend_function_entry php_protocolbuffers_unknown_field_methods[] = {
 	PHP_ME(protocolbuffers_unknown_field, getAsLengthDelimitedList, arginfo_pb_unknown_field_get_as_length_delimited, ZEND_ACC_PUBLIC)
 	PHP_ME(protocolbuffers_unknown_field, getAsFloatList, arginfo_pb_unknown_field_get_as_float_list, ZEND_ACC_PUBLIC)
 	PHP_ME(protocolbuffers_unknown_field, getAsDoubleList, arginfo_pb_unknown_field_get_as_float_list, ZEND_ACC_PUBLIC)
-    	PHP_ME(protocolbuffers_unknown_field, getAsFixed32List, arginfo_pb_unknown_field_get_as_fixed32, ZEND_ACC_PUBLIC)
+	PHP_ME(protocolbuffers_unknown_field, getAsFixed32List, arginfo_pb_unknown_field_get_as_fixed32, ZEND_ACC_PUBLIC)
 	PHP_ME(protocolbuffers_unknown_field, getAsFixed64List, arginfo_pb_unknown_field_get_as_fixed64, ZEND_ACC_PUBLIC)
 	{NULL, NULL, NULL}
 };
