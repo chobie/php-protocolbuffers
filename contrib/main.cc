c4f40dbe489caff7520d01953488de75624db260
Shuhei Tanuma <shuhei.tanuma@gmail.com>
2013-12-17 07:33:53 +0900

    print versions as its easy to determine the plugin

--- contrib/main.cc
+++ contrib/main.cc
@@ -4,8 +4,12 @@
 
 #include "google/protobuf/compiler/php/php_generator.h"
 
+#include <iostream>
+
 int main(int argc, char* argv[]) {
     google::protobuf::compiler::php::PHPGenerator generator;
+
+    std::cerr << "protoc-gen-php 0.1.0 by chobie\n" << std::endl;
 
     return PluginMain(argc, argv, &generator);
 }
