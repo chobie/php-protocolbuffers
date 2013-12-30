0522b19a548f4bb6b1fa1d489a0c730e90160d2f
Shuhei Tanuma <shuhei.tanuma@gmail.com>
2013-12-30 15:28:12 +0900

    forget to add files

---
+++ enum.c
@@ -0,0 +1,16 @@
+#include "protocolbuffers.h"
+#include "unknown_field_set.h"
+#include "unknown_field.h"
+
+static zend_function_entry protocolbuffers_enum_methods[] = {
+	PHP_FE_END
+};
+
+void php_protocolbuffers_enum_class(TSRMLS_D)
+{
+	zend_class_entry ce;
+
+	INIT_CLASS_ENTRY(ce, "ProtocolBuffersEnum", protocolbuffers_enum_methods);
+	php_protocol_buffers_enum_class_entry = zend_register_internal_class(&ce TSRMLS_CC);
+	PHP_PROTOCOLBUFFERS_REGISTER_NS_CLASS_ALIAS(PHP_PROTOCOLBUFFERS_NAMESPACE, "Enum", php_protocol_buffers_enum_class_entry);
+}
