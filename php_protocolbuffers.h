218364033334dd8f8acbbced415749bef3f03fd4
Shuhei Tanuma <shuhei.tanuma@gmail.com>
2014-02-14 05:01:31 +0900

    [added] * added IDE friendly stub file. * added ProtocolBuffersEnum::getName($value) method.
    [bug fixes]
    * #31 fixes potential for segmentation fault when using camel case field
    * name.
    * #32 don't waste memory when retain parent message.

--- php_protocolbuffers.h
+++ php_protocolbuffers.h
@@ -32,7 +32,7 @@
 #define PHP_PROTOCOLBUFFERS_H
 
 #define PHP_PROTOCOLBUFFERS_EXTNAME "protocolbuffers"
-#define PHP_PROTOCOLBUFFERS_VERSION "0.2.1"
+#define PHP_PROTOCOLBUFFERS_VERSION "0.2.2"
 
 #ifdef HAVE_CONFIG_H
 #include "config.h"
